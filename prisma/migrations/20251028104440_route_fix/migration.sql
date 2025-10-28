822006e2fb68ad59bc25037e4ab452dc93b56bb0
Kunal Daharwal <kunaldaharwal.h@gmail.com>
2025-10-28 16:18:24 +0530

    migration/fix/local

---
+++ prisma/migrations/20251028104440_route_fix/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "ContactMessage" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ContactMessage_pkey" PRIMARY KEY ("id")
+);
