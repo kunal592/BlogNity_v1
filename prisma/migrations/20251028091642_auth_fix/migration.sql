165cc5af223d0b79a3c9efbebb5124787d0f50a0
Kunal Daharwal <kunaldaharwal.h@gmail.com>
2025-10-28 14:47:09 +0530

    fix/9/local

---
+++ prisma/migrations/20251028091642_auth_fix/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `profileImage` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "profileImage",
+ADD COLUMN     "image" TEXT,
+ALTER COLUMN "email" DROP NOT NULL;
