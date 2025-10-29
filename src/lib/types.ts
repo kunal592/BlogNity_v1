99081daf306875456ce3ff7fdebcda43d447c9fa
kunaldaharwal.h <kunaldaharwal.h@gmail.com>
2025-10-29 21:20:01 +0000

    fix/37/4

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,28 @@
-import { Post as PrismaPost, User as PrismaUser, Comment as PrismaComment } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 
-export interface Comment extends PrismaComment {
-  author: PrismaUser;
-}
+// Manually define the Post type with all the expected relations
+export type Post = Prisma.PostGetPayload<{
+  include: {
+    author: true,
+    comments: {
+      include: {
+        author: true
+      }
+    },
+    likedBy: true,
+  }
+}> & { tags: string[] }; // Ensure tags is always an array of strings
 
-export interface Post extends PrismaPost {
-  author: PrismaUser;
-  comments: Comment[];
-  tags: { tag: { name: string } }[];
-}
-
-export interface User extends PrismaUser {}
+// Manually define the User type with all the expected relations
+export type User = Prisma.UserGetPayload<{
+  include: {
+    posts: true,
+    comments: true,
+    likedPosts: true,
+    followers: true,
+    following: true,
+    notifications: true,
+    sentNotifications: true,
+    bookmarkedPosts: true,
+  }
+}>;
